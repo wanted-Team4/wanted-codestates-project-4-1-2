397ddc8f58a59e164f74882b77b21edadc4c69e7
HeeYeonKim98 <wow9896@naver.com>
2022-02-24 17:11:29 +0900

    :bug: Fix : 버그 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Home from "./pages/home";
 import Navigation from "./components/Navigation";
-import { Provider } from "react-redux";
 import React from "react";
-import store from "./store/configure";
 
 const App = () => {
   return (
-    <Provider store={store}>
+    <>
       <Navigation />
       <Home />
-    </Provider>
+    </>
   );
 };
 
